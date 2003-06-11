97e0df3eb937c926f876a3f03096f9f6bbb570b1
davej <davej>
2003-06-11 23:44:21 +0000

    use defines for MSR numbers.

--- Intel/Intel.h
+++ Intel/Intel.h
@@ -10,4 +10,11 @@ void decode_microcode(struct cpudata *cpu);
 
 #define MSR_IA32_PLATFORM_ID	0x17
 #define MSR_IA32_UCODE_REV		0x8b
+
+#define MSR_IA32_PERF_STATUS		0x198
+#define MSR_IA32_PERF_CTL		0x199
+#define MSR_IA32_THERM_STATUS		0x19c
+#define MSR_PM_THERM2_CTL		0x19d
+#define MSR_IA32_MISC_ENABLE		0x1a0
+
 #endif /* _INTEL_H */
--- Intel/MSR-PM.c
+++ Intel/MSR-PM.c
@@ -1,5 +1,5 @@
 /*
- *  $Id: MSR-PM.c,v 1.2 2003/06/11 23:17:51 davej Exp $
+ *  $Id: MSR-PM.c,v 1.3 2003/06/11 23:44:21 davej Exp $
  *  This file is part of x86info.
  *  (C) 2002 Dave Jones.
  *
@@ -14,6 +14,7 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include "../x86info.h"
+#include "Intel.h"
 
 void dump_centrino_MSRs (struct cpudata *cpu)
 {
@@ -25,7 +26,7 @@ void dump_centrino_MSRs (struct cpudata *cpu)
 
 	printf ("Pentium M MSRs:\n");
 
-	if (read_msr (cpu->number, 0x198, &val)==1) {
+	if (read_msr (cpu->number, MSR_IA32_PERF_STATUS, &val)==1) {
 		/*
 		   Voltage and frequency values derived from 1300MHz
 		   Pentium M in an IBM ThinkPad X31.  Constants for
@@ -44,7 +45,7 @@ void dump_centrino_MSRs (struct cpudata *cpu)
 		printf ("  Current performance mode is 0x%04x: %dMHz, %d.%dV\n",
 			uv, mhz, volt/1000, volt%1000);
 	}
-	if (read_msr (cpu->number, 0x1a0, &val)==1) {
+	if (read_msr (cpu->number, MSR_IA32_MISC_ENABLE, &val)==1) {
 		printf ("  Enabled: ");
 		if (val & (1<<3)) {
 			printf("TCC ");
@@ -63,7 +64,7 @@ void dump_centrino_MSRs (struct cpudata *cpu)
 
 		printf("\n");
 	}
-	if (tcc && read_msr (cpu->number, 0x19d, &val)==1) { /* THERM2_CTL */
+	if (tcc && read_msr (cpu->number, MSR_PM_THERM2_CTL, &val)==1) { /* THERM2_CTL */
 		printf("  Thermal monitor %d\n", (val & (1<<16)) ? 2 : 1);
 	}
 	if (read_msr (cpu->number, 0x19a, &val)==1) { /* THERM_CONTROL */
@@ -73,7 +74,7 @@ void dump_centrino_MSRs (struct cpudata *cpu)
 		} else
 			printf("  Software-controlled clock disabled (full speed)\n");
 	}
-	if (read_msr (cpu->number, 0x19c, &val)==1) { /* THERM_STATUS */
+	if (read_msr (cpu->number, MSR_IA32_THERM_STATUS, &val)==1) { /* THERM_STATUS */
 		printf("  Thermal status: ");
 		if (val & (1<<0))
 			printf("TooHot ");
