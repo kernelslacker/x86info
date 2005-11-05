6b06bd5fa06860378fc57ed68573b7ebab2a25bf
Dave Jones <davej@redhat.com>
2005-11-04 19:03:58 -0500

    Fix up logic bugs, and enhance MSR-P4
    Spotted by <sirspiritus@yandex.ru>

--- Intel/MSR-P4.c
+++ Intel/MSR-P4.c
@@ -29,7 +29,7 @@ void dump_p4_MSRs (struct cpudata *cpu)
 
 	if (read_msr (cpu->number, 0x2a, &val)==1) {
 		printf ("System bus in order queue depth=");
-		if ((val & (1<<2))==1)
+		if ((val & (1<<2)))
 			printf ("1");
 		else
 			printf ("12");
@@ -46,7 +46,7 @@ void dump_p4_MSRs (struct cpudata *cpu)
 
 	if (read_msr (cpu->number, 0x119, &val)==1) {
 		printf ("Processor serial number is ");
-		if ((val & (1<<21))==1)
+		if ((val & (1<<21)))
 			printf ("dis");
 		else
 			printf ("en");
@@ -54,33 +54,60 @@ void dump_p4_MSRs (struct cpudata *cpu)
 	}
 
 	if (read_msr (cpu->number, 0x1a0, &val)==1) {
-		printf ("Fast strings are ");
-		if ((val & (1<<0))==0)
-			printf ("dis");
-		else
-			printf ("en");
-		printf ("abled\n");
+		printf ("Fast string enable is ");
+		if (!(val & (1<<0)))
+			printf ("un");
+		printf ("set\n");
 
 		printf ("x87 FPU Fopcode compatability mode is ");
-		if ((val & (1<<2))==0)
-			printf ("dis");
-		else
-			printf ("en");
-		printf ("abled\n");
+		if (!(val & (1<<2)))
+			printf ("un");
+		printf ("set\n");
 
-		printf ("Thermal monitor is ");
-		if ((val & (1<<3))==0)
-			printf ("dis");
-		else
-			printf ("en");
-		printf ("abled\n");
+		printf ("Thermal monitor enable is ");
+		if (!(val & (1<<3)))
+			printf ("un");
+		printf ("set\n");
 
-		printf ("Split lock is ");
-		if ((val & (1<<4))==1)
-			printf ("dis");
-		else
-			printf ("en");
-		printf ("abled\n");
+		printf ("Split lock disable is ");
+		if (!(val & (1<<4)))
+			printf ("un");
+		printf ("set\n");
+
+		printf ("L3 cache disable is ");
+		if (!(val & (1<<6)))
+			printf ("un");
+		printf ("set\n");
+
+		printf ("Performance monitoring is ");
+		if (!(val & (1<<7)))
+			printf ("un");
+		printf ("available\n");
+
+		printf ("Suppress lock enable is ");
+		if (!(val & (1<<8)))
+			printf ("un");
+		printf ("set\n");
+
+		printf ("Prefetch queue disable is ");
+		if (!(val & (1<<9)))
+			printf ("un");
+		printf ("set\n");
+
+		printf ("FERR# Interrupt reporting enable is ");
+		if (!(val & (1<<10)))
+			printf ("un");
+		printf ("set\n");
+
+		printf ("Branch trace storage unavailable is ");
+		if (!(val & (1<<11)))
+			printf ("un");
+		printf ("set\n");
+
+		printf ("Precise Event Based Sampling Unavailable is ");
+		if (!(val & (1<<12)))
+			printf ("un");
+		printf ("set\n");
 
 	}
 /*
