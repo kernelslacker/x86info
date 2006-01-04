f5805b4546032efe673f65f1b349123d4744fec8
Dave Jones <davej@redhat.com>
2006-01-04 18:28:53 -0500

    Bump year.

--- x86info.c
+++ x86info.c
@@ -178,7 +178,7 @@ int main (int argc, char **argv)
 
 	parse_command_line(argc, argv);
 	if (!silent) {
-		printf ("x86info v1.17.  Dave Jones 2001-2005\n");
+		printf ("x86info v1.17.  Dave Jones 2001-2006\n");
 		printf ("Feedback to <davej@redhat.com>.\n\n");
 	}
 
