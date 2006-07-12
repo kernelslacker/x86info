b4bd292722b5710d3e5763e73ed7bc1ce48e78af
Dave Jones <davej@redhat.com>
2006-07-12 17:29:45 -0400

    1.18

--- x86info.c
+++ x86info.c
@@ -179,7 +179,7 @@ int main (int argc, char **argv)
 
 	parse_command_line(argc, argv);
 	if (!silent) {
-		printf ("x86info v1.17.  Dave Jones 2001-2006\n");
+		printf ("x86info v1.18.  Dave Jones 2001-2006\n");
 		printf ("Feedback to <davej@redhat.com>.\n\n");
 	}
 
