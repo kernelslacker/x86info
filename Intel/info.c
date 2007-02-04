71cc3ebc70c70d8d54d35e2588a1eb3ee3b30524
Stanislav Sedov <stas@FreeBSD.org>
2007-02-04 13:56:36 -0500

    Intel model 13 is centrino too

--- Intel/info.c
+++ Intel/info.c
@@ -69,7 +69,7 @@ void display_Intel_info (struct cpudata *cpu)
 	if (show_msr) {
 		if (cpu->family==0xf)
 			dump_p4_MSRs(cpu);
-		if (cpu->family==0x6 && cpu->model==9)
+		if (cpu->family==0x6 && (cpu->model == 9 || cpu->model == 13))
 			dump_centrino_MSRs(cpu);
 	}
 
