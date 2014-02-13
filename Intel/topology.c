5088388923c09d153f2bf1acd51d83674a465fa2
Dave Jones <davej@redhat.com>
2014-02-12 19:23:45 -0500

    Use updated version of fls from the Linux kernel

--- Intel/topology.c
+++ Intel/topology.c
@@ -22,31 +22,7 @@
 
 static int fls(int x)
 {
-	int r = 32;
-
-	if (!x)
-		return 0;
-	if (!(x & 0xffff0000u)) {
-		x <<= 16;
-		r -= 16;
-	}
-	if (!(x & 0xff000000u)) {
-		x <<= 8;
-		r -= 8;
-	}
-	if (!(x & 0xf0000000u)) {
-		x <<= 4;
-		r -= 4;
-	}
-	if (!(x & 0xc0000000u)) {
-		x <<= 2;
-		r -= 2;
-	}
-	if (!(x & 0x80000000u)) {
-		x <<= 1;
-		r -= 1;
-	}
-	return r;
+	return x ? sizeof(x) * 8 - __builtin_clz(x) : 0;
 }
 
 static int get_count_order(unsigned int count)
