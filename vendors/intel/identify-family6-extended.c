080f45262f06038fba8644050228615d7221de18
Dave Jones <davej@codemonkey.org.uk>
2015-07-09 20:06:38 -0400

    Add skylake

--- vendors/intel/identify-family6-extended.c
+++ vendors/intel/identify-family6-extended.c
@@ -384,6 +384,10 @@ SLGAS   723  m-FCBGA M-0 1.20/(n/a)/(n/a) 800 N/A  10   1    (Celeron)
 		add_to_cpuname("[Knights Landing]");
 		break;
 
+	case 94:
+		add_to_cpuname("[Skylake]");
+		break;
+
 	default:
                 add_to_cpuname("Unknown model. ");
 	}
